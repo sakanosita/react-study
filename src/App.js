d31e08a2f82405be5d3b4c29e1e1e50ec5e33e2c
Moto Sakanosita <moto@sakanosita.org>
2021-12-16 18:39:20 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Article from "./components/Article";
 
 function App() {
+  const now = new Date();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Article
+        title={"hoge"}
+        content={"fuga"}
+        updatedAt={now}
+      />
     </div>
   );
 }
---
+++ src/components/Article.js
@@ -0,0 +1,11 @@
+const Article = (props) => {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <p>{props.content}</p>
+            <p>{props.updatedAt.toString()}</p>
+        </div>
+    );
+}
+
+export default Article;
